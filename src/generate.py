e780033ec74fe2fed6c9e62f355414f7ccbec0a8
Aculisme <luca.mehl@gmail.com>
2019-01-11 14:38:10 +0100

    added username functionality

--- src/generate.py
+++ src/generate.py
@@ -1,16 +1,24 @@
 from textgenrnn import textgenrnn
 import sys, datetime as d
 
+u = bool(input("Would you like to generate usernames? "))
 subreddit = str(input("Choose a subreddit: "))
-comment_limit = int(input("How many comments should it generate? "))
+comment_limit = int(input("How many strings should it generate? "))
 # temperature = int(input("At what Temperature (0.2-1)? "))
 
 try:
-    textgen = textgenrnn('models/'+subreddit+'.hdf5')
+    if u == True:
+        textgen = textgenrnn('models/u_'+subreddit+'.hdf5')
+    else:
+        textgen = textgenrnn('models/'+subreddit+'.hdf5')
 except:
     sys.exit("\nSorry, we couldn't find "+subreddit+" in the models folder. Please try again.")
 
 a = d.datetime.now()
-textgen.generate_to_file('outputs/'+subreddit+'.txt',n=comment_limit,max_gen_length=100) # , temperature=0.5
+# textgen.generate_to_file('outputs/'+subreddit+'.txt',n=comment_limit,max_gen_length=100) # , temperature=0.5
+if u == True:
+    textgen.generate_to_file('outputs/u_'+subreddit+'.txt',n=comment_limit) # , temperature=0.5
+else:
+    textgen.generate_to_file('outputs/'+subreddit+'.txt',n=comment_limit) # , temperature=0.5
 b = d.datetime.now()
 sys.exit("\nDone. "+str(comment_limit)+" comments generated in "+str(round((b-a).total_seconds(),2))+" seconds.")
--- src/train.py
+++ src/train.py
@@ -2,6 +2,8 @@ import math, sys
 from textgenrnn import textgenrnn
 from pathlib import Path
 
+################## TODO: change inputs to flags
+u = bool(input("Would you like to train usernames? "))
 subreddit = str(input("Choose a subreddit: "))
 n_epochs = int(input("How many epochs would you like the model to train for? "))
 
@@ -10,13 +12,23 @@ if n_epochs<2:
 else:
     save_incr = 2
 
-input_location = 'inputs/in_'+subreddit+'.txt'
-model_location = 'models/'+subreddit+'.hdf5'
+if u == True:
+    input_location = 'u_inputs/u_in_'+subreddit+'.txt' # (uin_)                      fine
+    model_location = 'models/u_'+subreddit+'.hdf5' # change                         fine (it'll go to uall now) 
+    # search_model_location = Path("models/u"+subreddit+'.hdf5') # change          
 
-my_file = Path("models/"+subreddit+'.hdf5')
-if my_file.is_file():
+else:
+    input_location = 'inputs/in_'+subreddit+'.txt' # (in_)
+    model_location = 'models/'+subreddit+'.hdf5' # change
+    # search_model_location = Path("models/"+subreddit+'.hdf5') # change
+
+search_model_location = Path(model_location)
+
+if search_model_location.is_file():
+    print("Training from existing model")
     textgen = textgenrnn(model_location)
 else:
+    print("Training new model")
     textgen = textgenrnn()
 
 print("\nTraining for: ",n_epochs," epochs, and saving every ",save_incr," epochs.")
